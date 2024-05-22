baf3e1181f0c81ba5486441f693a0a68b2253e18
Mingyung Park <kateking001130@ewhain.net>
2024-05-22 20:09:39 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+# 이동할 디렉토리를 설정합니다.
 cd ../
-mkdir output
+
+# output 디렉토리를 생성합니다.
+mkdir -p output
+
+# frontend-server 디렉토리의 모든 파일을 output 디렉토리로 복사합니다.
 cp -R ./frontend-server/* ./output
-cp -R ./output ./frontend-server/
